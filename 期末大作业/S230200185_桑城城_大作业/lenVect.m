fccb3f805c004d64bcf974b37205de4a03061e3c
HnuAiSimOpt <seminarwh@163.com>
2024-05-21 17:40:25 +0800

    del

--- 期末大作业/S230200185_桑城城_大作业/lenVect.m
+++
@@ -1,5 +0,0 @@
-function [lenVect] = lenVect(Vect)
-%%%% nPointGen = 需要生成的材料点数
-lenVect = (Vect(1,1)^2+Vect(1,2)^2+Vect(1,3)^2)^(1/2);
-end
-
--- 期末大作业/S230200185_桑城城_大作业/solution.m
+++
@@ -1,5 +0,0 @@
-function disp = solution(nDof,fixedDof,K,force)
-activeDof = setdiff((1:nDof)',fixedDof);
-U = K(activeDof,activeDof)\force(activeDof);
-disp = zeros(nDof,1);
-disp(activeDof) = U;
